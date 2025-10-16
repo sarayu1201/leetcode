3c815d45753bf4c65d0d58515f307107cbed3de9
sarayu1201 <23P31A1201@acet.ac.in>
2025-10-16 21:05:04 +0530

    Time: 24 ms (81.91%), Space: 70.8 MB (99.61%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int last = nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(last==nums[i]){
+                return true;
+            }
+            else{
+                last = nums[i];
+            }
+        }
+        return false;
+    }
+};
